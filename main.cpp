b12e5929133f9889b942933710465fccf27235f8
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-10 15:33:05 +0200

    created code

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main();
+{
+	cout << "\t" << endl;
+
+	cout << "\t  Name\t      :\t\The war and the peace" << endl;
+		<< "\t  Avtor \t :\t\ L.N. Tolstoj" << endl;
+		<< "\t  Izdatelstvo\t :\t\ Piter" << endl;
+		<< "\t  Pages \t : \t\ 500." << endl;
+
+	cout << "\t" << endl;
+	cout << "\t" << endl;
+
+
+	cout << "\t  \"Kraj lukomor’a dub zelenyj," << endl;
+		<< "\t  I zolotyj lancyuh na nim:" << endl;
+		<< "\t  Shodnya, shonochi kit uchenyj" << endl;
+		<< "\t  Na lancyuhu kruzhl’aje tim”" << endl; 
+	return 0;
+}
+
