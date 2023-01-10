58fbccc6996fa0881449e554ed93705fc6045307
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-10 15:34:00 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
-int main();
+
+int main()
 {
 	cout << "\t" << endl;
 
-	cout << "\t  Name\t      :\t\The war and the peace" << endl;
-		<< "\t  Avtor \t :\t\ L.N. Tolstoj" << endl;
-		<< "\t  Izdatelstvo\t :\t\ Piter" << endl;
-		<< "\t  Pages \t : \t\ 500." << endl;
+	cout<< "\t  Name\t      :\t\The war and the peace" << endl;
+	cout<< "\t  Avtor \t :\t\ L.N. Tolstoj" << endl;
+	cout<< "\t  Izdatelstvo\t :\t\ Piter" << endl;
+	cout<< "\t  Pages \t : \t\ 500." << endl;
 
 	cout << "\t" << endl;
 	cout << "\t" << endl;
 
 
 	cout << "\t  \"Kraj lukomor’a dub zelenyj," << endl;
-		<< "\t  I zolotyj lancyuh na nim:" << endl;
-		<< "\t  Shodnya, shonochi kit uchenyj" << endl;
-		<< "\t  Na lancyuhu kruzhl’aje tim”" << endl; 
+	cout<< "\t  I zolotyj lancyuh na nim:" << endl;
+	cout<< "\t  Shodnya, shonochi kit uchenyj" << endl;
+	cout<< "\t  Na lancyuhu kruzhl’aje tim”" << endl; 
 	return 0;
 }
 
